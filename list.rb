48dae19cf8776f4f8533773db464802ea85693f7
Jarmoljuk Taras <jarmoljuktaras@gmail.com>
2012-11-14 18:00:26 +0200

    init class list

--- list.rb
+++ list.rb
@@ -1,1 +1,11 @@
-puts "hello world"
+class Element
+	attr_accessor :first,:last,:count
+	
+	def initialize first = nil, last = nil, count = 0
+		self.first = first
+		self.last = last
+		self.count = count
+	end
+end
+
+	 
