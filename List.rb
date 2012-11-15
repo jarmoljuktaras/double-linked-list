b9d2f69c029baccb66131da3d3ffe65055f5d8c2
Jarmoljuk Taras <jarmoljuktaras@gmail.com>
2012-11-15 11:40:27 +0200

    git initialize

---
+++ List.rb
@@ -0,0 +1,104 @@
+class Node
+	attr_accessor :value, :prev, :nextnode
+	def initialize (value,prev,nextnode)
+		@value = value
+		@prev = prev 
+		@nextnode = nextnode
+	end
+		
+	def to_s
+		if(@value == nil)
+			return nil;
+		else
+			return @value.to_s
+		end
+	end
+end
+
+class List
+	attr_accessor :head, :tail, :size
+
+	def initialize
+		@size=0
+	end
+	
+	def addNode (value)
+		if @head.nil?
+			@head=@tail=Node.new(value,nil,nil)
+		else
+			@tail.nextnode=Node.new(value,@tail,nil)
+			@tail=tail.nextnode
+		end
+		@size+=1
+	end
+	
+	def delNode (number)
+		if(number>size || number<1)
+			puts "have not such node"
+		else
+			n=1
+			node=@head
+			while n<number
+				node=node.nextnode
+			n+=1
+			end
+			if node.prev == nil
+				if (size == 1)
+					@head=nil
+					@tail=nil
+				else
+					node.nextnode.prev=nil
+					@head=node.nextnode
+				end
+				@size-=1
+				puts "Element " + number.to_s+" deleted"
+			end
+			if node.nextnode == nil
+				node.prev.nextnode=nil
+				@tail=node.prev	
+				@size-=1	
+				puts "Element " + number.to_s+" deleted"
+			end
+			if node.prev != nil && node.nextnode != nil
+				node.prev.nextnode=node.nextnode
+				node.nextnode.prev=node.prev
+				@size-=1
+				puts "Element " + number.to_s+" deleted"
+			end
+		end
+	end
+					
+
+	def to_s
+	
+		return "" unless @head
+		node=@head
+		result = "#{@head.value}"
+		result +=" #{node.value}" while node=node.nextnode
+		result
+	end
+end
+
+list = List.new
+list.addNode(8)
+list.addNode(14)
+list.addNode(15)
+list.addNode(17)
+list.addNode(19)
+list.addNode(119)
+puts list
+list.delNode(3)
+puts list			
+
+
+
+
+
+
+
+
+
+
+
+
+
